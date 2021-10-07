dffe72e7752ea9c232223b707458dbe7fb4908a1
DumbMahreeo <massi.noviello@gmail.com>
2021-10-07 22:22:50 +0200

    Adding yarv/full feature

--- src/exec.rs
+++ src/exec.rs
@@ -1,8 +1,14 @@
 use std::process::Command;
 use crate::filesystem::read_file;
 
+#[cfg(not(any(feature = "yarv", feature="full")))]
+const EXECUTABLE: &str = "mruby";
+
+#[cfg(any(feature = "yarv", feature="full"))]
+const EXECUTABLE: &str = "ruby";
+
 pub fn execute_code(code: &str) -> String {
-    let output = Command::new("mruby")
+    let output = Command::new(EXECUTABLE)
         .arg("-e")
         .arg(code)
         .output()
@@ -12,11 +18,5 @@ pub fn execute_code(code: &str) -> String {
 }
 
 pub fn execute_file(file: &str) -> String {
-    let output = Command::new("mruby")
-        .arg("-e")
-        .arg(read_file(file))
-        .output()
-        .expect("Couldn't get output from mruby");
-
-    String::from_utf8(output.stdout).expect("Non utf-8 output from mruby")
+    execute_code(&read_file(file))
 }
